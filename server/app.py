b8f7a123f95c1471c3d25c9457570dfeb4d6b72a
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-04-12 17:02:08 +0300

    Maden the correct changes

--- server/app.py
+++ server/app.py
@@ -28,16 +28,41 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_name=request.get_json()["username"]
+        print(user_name)
+        user=User.query.filter(User.username==user_name).first()
+        user_password=request.get_json().get("password")
+        if User.authenticate(user_password):
+            user_dict=user.to_dict()
+            return user_dict,200
+        else:
+            return {},204         
+
+
 
 class Login(Resource):
-    pass
-
+    def post(self):
+        username=request.get_json().get("username")
+        user=User.query.filter(User.username==username).first()        
+        if user:
+            session["user_id"]=user.id
+            return user.to_dict(),201
+        else:
+            return {},204
 class Logout(Resource):
-    pass
+    def delete(self):
+
+        session['user_id'] = None
+        
+        return {}, 204   
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login,"/login",endpoint="login")
+api.add_resource(Logout,"/logout",endpoint="logout")
+api.add_resource(CheckSession,"/check_session",endpoint="check_session")
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
